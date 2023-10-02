08c58f62d314680eeb5dce2796e38746f6bec62e
CovenantJunior <covenanttope@gmail.com>
2023-10-02 11:07:59 +0100

    multer in the building

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const multer  = require('multer');
+const app = express();
+const upload = multer({ dest: 'audios/' });
 const path = require('path');
 const app = express();
 const port = 3000;
+const censor = require('./censor');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+app.get('/censor', (req, res) => {
+
+})
