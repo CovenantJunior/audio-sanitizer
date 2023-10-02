55d6629e502b2df043270765144a630cf3c30618
CovenantJunior <covenanttope@gmail.com>
2023-10-02 11:57:16 +0100

    google-cloud/storage

--- server.js
+++ server.js
@@ -9,6 +9,36 @@ const speech = require('@google-cloud/speech');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
+/**
+ * TODO(developer):
+ *  1. Uncomment and replace these variables before running the sample.
+ *  2. Set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
+ *  3. Make sure you have the necessary permission to list storage buckets "storage.buckets.list"
+ *    (https://cloud.google.com/storage/docs/access-control/iam-permissions#bucket_permissions)
+ */
+// const projectId = 'YOUR_PROJECT_ID';
+
+const {Storage} = require('@google-cloud/storage');
+
+async function authenticateImplicitWithAdc() {
+  // This snippet demonstrates how to list buckets.
+  // NOTE: Replace the client created below with the client required for your application.
+  // Note that the credentials are not specified when constructing the client.
+  // The client library finds your credentials using ADC.
+  const storage = new Storage({
+    projectId,
+  });
+  const [buckets] = await storage.getBuckets();
+  console.log('Buckets:');
+
+  for (const bucket of buckets) {
+    console.log(`- ${bucket.name}`);
+  }
+
+  console.log('Listed all storage buckets.');
+}
+
+authenticateImplicitWithAdc();
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,13 +53,14 @@ app.post('/censor', upload.single('audio'), (req, res) => {
 
     // Path to the audio file
     let filePath = req.file.path;
+    // console.log(filePath);
 
     // Convert audio file to mono channel and save as .flac
     ffmpeg(filePath)
         .audioChannels(1)
         .audioFrequency(16000)
         .format('flac')
-        .save('output.flac')
+        .save('outputs/output.flac')
         .on('end', async () => {
             // The audio file is now mono channel and in .flac format
 
@@ -37,7 +68,7 @@ app.post('/censor', upload.single('audio'), (req, res) => {
             const client = new speech.SpeechClient();
 
             // Reads a local audio file and converts it to base64
-            const file = fs.readFileSync('output.flac');
+            const file = fs.readFileSync('outputs/output.flac');
             const audioBytes = file.toString('base64');
 
             // The audio file's encoding, sample rate in hertz, and BCP-47 language code
